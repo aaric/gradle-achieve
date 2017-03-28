d21dcf59795e2c575946d254e6edfeb4128f495a
Aaric <vipaaric@gmail.com>
2017-03-28 22:20:07 +0800

    Update jetties version.

--- src/main/java/com/github/aaric/abs/AbstractControllerObject.java
+++ src/main/java/com/github/aaric/abs/AbstractControllerObject.java
@@ -1,5 +1,20 @@
 package com.github.aaric.abs;
 
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import com.github.aaric.utils.IpUtils;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 /**
@@ -16,11 +31,119 @@ public abstract class AbstractControllerObject {
 	protected Logger logger;
 
 	/**
+	 * 注入Web容器ServletContext对象
+	 */
+	@Autowired
+	private ServletContext servletContext;
+
+	/**
 	 * 默认构造函数
 	 */
 	public AbstractControllerObject() {
 		super();
 		logger = Logger.getLogger(this.getClass());
+	}
+
+	/**
+	 * 获得ServletContext对象
+	 * 
+	 * @return
+	 * @author Aaric
+	 * @since 2015-11-12
+	 */
+	public ServletContext getServletContext() {
+		return servletContext;
+	}
+
+	/**
+	 * 获得HttpServletRequest对象
+	 * 
+	 * @return
+	 * @author Aaric
+	 * @since 2015-11-12
+	 */
+	public HttpServletRequest getRequest() {
+		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+	}
+
+	/**
+	 * 获得HttpServletResponse对象
+	 * 
+	 * @return
+	 * @author Aaric
+	 * @since 2015-12-01
+	 */
+	public HttpServletResponse getResponse() {
+		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
+	}
+
+	/**
+	 * 根据Cookie名字获得Cookie的值
+	 * 
+	 * @param cookieName
+	 *            名称
+	 * @param cookieValue
+	 *            字符串
+	 * @return
+	 * @author Aaric
+	 * @since 2015-12-16
+	 */
+	public Cookie setCookieValue(String cookieName, String cookieValue) {
+		Cookie cookie = new Cookie(cookieName, cookieValue);
+		cookie.setPath("/");
+		this.getResponse().addCookie(cookie);
+		return cookie;
+	}
+
+	/**
+	 * 根据Cookie名字获得Cookie的值
+	 * 
+	 * @param cookieName
+	 *            名称
+	 * @return
+	 * @author Aaric
+	 * @since 2015-12-16
+	 */
+	public String getCookieValue(String cookieName) {
+		if (StringUtils.isNotBlank(cookieName)) {
+			Cookie[] cookies = getRequest().getCookies();
+			if (null != cookies && 0 != cookies.length) {
+				for (Cookie cookie : cookies) {
+					if (cookieName.equals(cookie.getName())) {
+						return cookie.getValue();
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 获得Locale对象(国际化)
+	 * 
+	 * @return
+	 * @author Aaric
+	 * @since 2015-11-12
+	 */
+	public Locale getLocale() {
+		Locale returnValue = (Locale) getRequest().getSession()
+				.getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME);
+		if (null == returnValue) {
+			returnValue = getRequest().getLocale();
+			getRequest().setAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME, returnValue);
+		}
+		return returnValue;
+	}
+
+	/**
+	 * 获得客户端真实IP地址字符串
+	 * 
+	 * @return
+	 * @author Aaric
+	 * @since 2015-11-12
+	 */
+	public String getClientIpString() {
+		return IpUtils.getClientIpString(getRequest());
 	}
 
 }
---
+++ src/test/java/com/github/aaric/utils/ShardedJedisPoolTest.java
@@ -0,0 +1,85 @@
+package com.github.aaric.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
+
+/**
+ * ShardedJedisPool测试类
+ * 
+ * @author Aaric
+ * @since 2017-03-25
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:applicationContext-Test.xml" })
+public class ShardedJedisPoolTest {
+
+	@Autowired
+	private ShardedJedisPool shardedJedisPool;
+
+	@Test
+	@Ignore
+	public void testString() throws Exception {
+		// 初始化
+		String keyName = "hello";
+		ShardedJedis shardedJedis = shardedJedisPool.getResource();
+		// 设置
+		shardedJedis.set(keyName, "hello");
+		// 拼接
+		shardedJedis.append(keyName, " world");
+		// 读取
+		System.out.println(shardedJedis.get(keyName));
+		// 删除
+		shardedJedis.del(keyName);
+		// 进行加1操作
+		shardedJedis.set("number", "1");
+		shardedJedis.incr("number");
+	}
+
+	@Test
+	@Ignore
+	public void testMap() throws Exception {
+		// 初始化
+		String keyName = "helloMap";
+		ShardedJedis shardedJedis = shardedJedisPool.getResource();
+		// 设置
+		Map<String, String> helloMap = new HashMap<String, String>();
+		helloMap.put("hello1", "hello1");
+		helloMap.put("hello2", "hello2");
+		helloMap.put("hello3", "hello3");
+		shardedJedis.hmset(keyName, helloMap);
+		// 删除
+		shardedJedis.hdel(keyName, "hello1");
+		// 键集合
+		System.out.println(shardedJedis.hkeys(keyName));
+		// 值集合
+		System.out.println(shardedJedis.hvals(keyName));
+		// 长度
+		System.out.println(shardedJedis.hlen(keyName));
+		// 读取
+		System.out.println(shardedJedis.hmget(keyName, "hello1", "hello2"));
+	}
+
+	@Test
+	public void testList() throws Exception {
+		// 初始化
+		String keyName = "helloList";
+		ShardedJedis shardedJedis = shardedJedisPool.getResource();
+		// 设置-顶部
+		shardedJedis.lpush(keyName, "hello1");
+		shardedJedis.lpush(keyName, "hello2");
+		shardedJedis.lpush(keyName, "hello3");
+		// 设置-尾部
+		shardedJedis.rpush(keyName, "hello4");
+	}
+
+}
